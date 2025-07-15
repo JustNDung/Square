b233c99863c8384e8f060dab2e42858aebd3922f
Dungnn <dungnn@falcongames.com>
2025-07-15 11:10:00 +0700

    Add Save Load system and implement character movement logic.

---
+++ Assets/Scripts/SaveLoad/PlayerData.cs
@@ -0,0 +1,7 @@
+﻿namespace SaveLoad
+{
+    public class PlayerData
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SaveLoad/ServerSaveLoadManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace SaveLoad
+{
+    public class ServerSaveLoadManager : MonoBehaviour
+    {
+        
+    }
+}
