0c8e73278e330c1dad69b0bb9022c0415bbdb055
Dungnn <dungnn@falcongames.com>
2025-08-20 10:43:00 +0700

    Improve find win paths to reduce alloc.

--- Assets/Scripts/Character/FindPathsToWin.cs
+++ Assets/Scripts/Character/FindPathsToWin.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-using System.Linq;
+using UnityEngine.Pool; // có từ Unity 2021
 
 public class FindPathsToWin
 {
@@ -9,76 +9,87 @@ public class FindPathsToWin
     private Dictionary<CharacterController, List<Vector3>> _winPaths = new Dictionary<CharacterController, List<Vector3>>();
     private Dictionary<CharacterController, Stack<Vector3>> _startPosPerState = new Dictionary<CharacterController, Stack<Vector3>>();
     private bool _isWin = false;
-    private List<Vector3> _directions = new List<Vector3>()
+
+    private Vector3[] _directions = new Vector3[]
     {
-        Vector3.right,
-        Vector3.forward,
         Vector3.left,
-        Vector3.back
+        Vector3.forward,
+        Vector3.back,
+        Vector3.right
     };
-    
+
+    private readonly Vector3 _offset = new Vector3(0, 0.25f, 0);
 
     public void FindAllMovePathToWin()
     {
-        MapState tempMState = MapManager.Instance.MapState;
-        
         if (_isWin) return;
 
-        if (tempMState.IsWin())
+        if (MapManager.Instance.MapState.IsWin())
         {
             _isWin = true;
             foreach (var character in _characters)
             {
-                _winPaths[character] = _currentPaths[character].SelectMany(x => x).ToList();
+                // dùng pool để giảm alloc thay vì ToList()
+                var pooledList = ListPool<Vector3>.Get();
+                foreach (var path in _currentPaths[character])
+                {
+                    pooledList.AddRange(path);
+                }
+                _winPaths[character] = pooledList;
             }
             return;
         }
 
         foreach (var character in _characters)
         {
-            _startPosPerState[character].Push(character.CurrentPosByCalculate); 
+            _startPosPerState[character].Push(character.CurrentPosByCalculate);
         }
+        
         foreach (var direction in _directions)
         {
             bool isStateChanged = false;
             foreach (var character in _characters)
             {
                 character.FindMovePaths(direction, character.CurrentPosByCalculate);
-
                 if (character.MovePaths.Count > 0)
                 {
                     isStateChanged = true;
                 }
-                
             }
-            
+
             if (isStateChanged)
             {
                 foreach (var character in _characters)
                 {
-                    _currentPaths[character].Add(new List<Vector3>(character.MovePaths)); 
+                    // dùng pool thay vì new List
+                    var pooledList = ListPool<Vector3>.Get();
+                    pooledList.AddRange(character.MovePaths);
+                    _currentPaths[character].Add(pooledList);
                 }
-                
+
                 FindAllMovePathToWin();
-                
+
                 foreach (var character in _characters)
                 {
                     int numberOfMovePaths = _currentPaths[character].Count;
-                    
                     if (numberOfMovePaths == 0) continue;
-                    
-                    for (int i = 0; i < _currentPaths[character][numberOfMovePaths - 1].Count; i++)
+
+                    var lastPath = _currentPaths[character][numberOfMovePaths - 1];
+                    for (int i = 0; i < lastPath.Count; i++)
                     {
-                        tempMState.UnvisitTile(_currentPaths[character][numberOfMovePaths - 1][i] - new Vector3(0, 0.25f, 0));
+                        MapManager.Instance.MapState.UnvisitTile(lastPath[i] - _offset);
                     }
-                    
+
+                    // Trả lại list vào pool
+                    ListPool<Vector3>.Release(lastPath);
+
                     _currentPaths[character].RemoveAt(numberOfMovePaths - 1);
 
                     if (_startPosPerState[character].Count > 0)
                     {
                         _startPosPerState[character].Pop();
                     }
-                    
+
                     if (_startPosPerState[character].Count > 0)
                     {
                         character.CurrentPosByCalculate = _startPosPerState[character].Peek();
@@ -87,7 +98,7 @@ public class FindPathsToWin
             }
         }
     }
-    
+
     public void AddCharacter(CharacterController character)
     {
         _characters.Add(character);
@@ -95,8 +106,7 @@ public class FindPathsToWin
         _winPaths.Add(character, new List<Vector3>());
         _startPosPerState.Add(character, new Stack<Vector3>());
     }
-    
+
     public List<CharacterController> Characters => _characters;
-    
     public Dictionary<CharacterController, List<Vector3>> WinPaths => _winPaths;
 }
